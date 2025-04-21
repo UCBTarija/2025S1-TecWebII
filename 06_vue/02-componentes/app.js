af6c17d24c4dcb5e5e26f41273994b058e7423f5
Ronal Guerrero <ronal.guerrero@gmail.com>
2025-04-21 16:35:47 -0400

    21/04

--- 06_vue/02-componentes/app.js
+++ 06_vue/02-componentes/app.js
@@ -1,11 +1,14 @@
 import { ref } from 'vue';
 
 export default {
-    data () {
+    data() {
         const contador = ref(0);
+        const productos = ref([]);
+
         return {
             message: 'Vue desde modulo! ',
             contador,
+            productos
         }
     },
     template: `
@@ -18,14 +21,36 @@ export default {
             <button @click="incrementar">Incrementar</button>            
             <button @click="contador++">Incrementar Directo</button>  
             <input type="text" v-model="contador" />          
+
+            <button @click="cargarProductos">Cargar Productos</button>
+            <ul>
+                <li v-for="producto in productos">
+                    {{ producto.nombre }} - {{ producto.id }}
+                </li>
+            </ul>
+
+            <table border="1">
+                <tr v-for="producto in productos">
+                    <td>{{ producto.nombre }}</td>
+                    <td>{{ producto.id }}</td>
+                </tr>
+            </table>
         </div>
     `,
     methods: {
-        saludar () {
+        saludar() {
             alert('Hola desde el metodo')
         },
-        incrementar () {
+        incrementar() {
             this.contador++;
         },
+        cargarProductos() {
+            this.productos = [
+                { id: 1, nombre: 'Producto 1' },
+                { id: 2, nombre: 'Producto 2' },
+                { id: 3, nombre: 'Producto 3' },
+                { id: 4, nombre: 'Producto 4' },
+            ];
+        }
     },
 }
