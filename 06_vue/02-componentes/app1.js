a5f3fd5c023e8ced9b601f28344ecbd88ef9086f
Ronal Guerrero <ronal.guerrero@gmail.com>
2025-04-28 18:28:21 -0400

    28/04

--- 06_vue/02-componentes/app1.js
+++ 06_vue/02-componentes/app1.js
@@ -4,10 +4,11 @@ import { Productos } from './productos.js';
 export default {
     data () {
         const contador = ref( 0 );
-
+        const clase_color = ref( 'background-color:red;' );
         return {
             message: 'Vue desde modulo! ',
             contador,
+            clase_color
         }
     },
     template: /*html */`
@@ -16,7 +17,7 @@ export default {
             hola desde template
             {{ message }} {{contador}}
 
-            <button @click="saludar">Saludar {{ contador }} </button>
+            <button @click="saludar" v-bind:class="clase_color">Saludar {{ contador }} </button>
             <button @click="incrementar">Incrementar</button>
             <button @click="contador++">Incrementar Directo</button>
             <input type="text" v-model="contador" />
@@ -24,8 +25,8 @@ export default {
 
             <productos titulo="Productos encontrados"></productos>
             <productos titulo="Articulos"></productos>
-            <productos></productos>
-            <productos></productos>
+            <productos  ></productos>
+            <productos @error="handleError"></productos>
         </div>
     `,
     methods: {
@@ -35,6 +36,9 @@ export default {
         incrementar () {
             this.contador++;
         },
+        handleError(errorMessage) {
+            alert(errorMessage);
+        }
     },
     components: {
         "productos": Productos
--- 06_vue/02-componentes/productos.js
+++ 06_vue/02-componentes/productos.js
@@ -1,11 +1,14 @@
 import { ref } from 'vue';
 
 const Productos = {
-    data() {
-        const productos = ref([]);
+    data () {
+        const productos = ref( [] );
         return {
             productos
         }
+    },
+    mounted () {
+        this.cargarProductos();
     },
     template: /*html */`
         <div>
@@ -20,16 +23,19 @@ const Productos = {
         </div>
     `,
     methods: {
-        cargarProductos() {
-            fetch('http://127.0.0.1:5000/articulos-api')
-                .then(response => response.json())
-                .then(data => {
-                    this.productos = data.map(item => ({
+        cargarProductos () {
+            fetch( 'http://127.0.0.1:5000/articulos-api' )
+                .then( response => response.json() )
+                .then( data => {
+                    this.productos = data.map( item => ( {
                         id: item.id,
                         nombre: item.nombre
-                    }));
-                })
-                .catch(error => console.error('Error:', error));
+                    } ) );
+                } )
+                .catch( error => {
+                    console.error( 'Error:', error );
+                    this.$emit( 'error', "No se pudo recuperar la lista de productos" );
+                } );
         }
     },
     props: {
@@ -38,6 +44,9 @@ const Productos = {
             default: 'Productos'
         }
     },
+    emits: {
+        error: 'error'
+    }
 }
 
 export {
